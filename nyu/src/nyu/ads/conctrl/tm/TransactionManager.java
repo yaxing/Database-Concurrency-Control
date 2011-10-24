221a8bbf3034f2f7b776962fba093f9369e26db9
Matthew Sarnak <sarnak@gmail.com>
2011-10-24 16:25:10 -0400

    Initial Commit

---
+++ nyu/src/nyu/ads/conctrl/tm/TransactionManager.java
@@ -0,0 +1,17 @@
+package nyu.ads.conctrl.tm;
+
+import java.util.*;
+
+/**
+ * Transaction Manager class
+ * 
+ * @author Matt
+ *
+ */
+
+public class TransactionManager {
+	private TransactionTable transTable;
+	
+	private TMProcessor proc;
+
+}
---
+++ nyu/src/nyu/ads/conctrl/tm/TransactionTable.java
@@ -0,0 +1,43 @@
+package nyu.ads.conctrl.tm;
+
+import java.util.*;
+/**
+ * Transaction Table class
+ * 
+ * Holds starting timestamp, current status, and name
+ * of all transactions.
+ * 
+ * @author Matt Sarnak
+ *
+ */
+public class TransactionTable {
+	private List<Transaction> TransactionList;
+	
+	public TransactionTable() {
+		TransactionList = new ArrayList<Transaction>();
+	}
+	
+	public void addTransaction(Transaction t) {
+		TransactionList.add(t);
+	}
+	
+	public void updateTransactionStatus(Integer number, int status)
+	{
+		for (Transaction t : TransactionList) {
+			if (t.getNumber() == number) {
+				t.setStatus(status);
+			}
+		}
+	}
+	
+	public Integer getTimestamp(Integer number)
+	{
+		for (Transaction t : TransactionList) {
+			if (t.getNumber() == number) {
+				return t.getTimestamp();
+			}
+		}
+		return null;
+	}
+	
+}
