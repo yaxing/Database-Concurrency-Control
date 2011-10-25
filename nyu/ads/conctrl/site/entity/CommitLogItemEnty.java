42d6c3f29c7660e75c5cae7fca0cdc7f99f75734
Yaxing Chen <yangcongknight@gmail.com>
2011-10-24 20:40:18 -0400

    modify variable scope

--- nyu/ads/conctrl/site/entity/CommitLogItemEnty.java
+++ nyu/ads/conctrl/site/entity/CommitLogItemEnty.java
@@ -7,13 +7,13 @@
 package nyu.ads.conctrl.site.entity;
 
 public class CommitLogItemEnty {
-	int transactionId;
-	String operation; //W/R
-	int resourceId;
-	String value;
+	public int transactionId;
+	public String operation; //W/R
+	public int resourceId;
+	public String value;
 	
-	public CommitLogItemEnty(int id, String op, int resId, String value) {
-		this.transactionId = id;
+	public CommitLogItemEnty(int tid, String op, int resId, String value) {
+		this.transactionId = tid;
 		this.operation = op;
 		this.resourceId = resId;
 		this.value = value;
