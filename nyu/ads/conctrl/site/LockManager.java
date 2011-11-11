984b7ecbc23c1c636accab0507717f1c94771470
Yaxing Chen <yangcongknight@gmail.com>
2011-11-11 01:24:13 -0500

    finished & tested LockManager::lock and related functions

--- nyu/ads/conctrl/site/LockManager.java
+++ nyu/ads/conctrl/site/LockManager.java
@@ -10,17 +10,47 @@ import nyu.ads.conctrl.entity.*;
  */
 public class LockManager {
 	
-	public HashMap<String, ArrayList<String>> locks; // current locks: 
-										  //"resource"=>"transaction id:1/0 (1: lock exclusive, 0: shared)"
+	private HashMap<String, HashMap<Integer, Integer>> locks; // lock table
+													//"resource"=>HashMap<transactionID, lockType>
+													//(1: lock exclusive, 0: shared)
 	
-	//public HashMap<String, Integer> locks;	// current locks: "resource"=>"transaction id"
-	
-	public HashMap<String, Integer> recoverLocks; // replicated resources that are locked from reading when site recover
+	private HashMap<String, Integer> recoverLocks; // replicated resources that are locked from reading when site recover
 												  // delete when certain resource is committed written
 												  // locked as -1
 	
 	public LockManager() {
-		locks = new HashMap<String, ArrayList<String>>(); 
+		locks = new HashMap<String, HashMap<Integer, Integer>>(); 
+		recoverLocks = new HashMap<String, Integer>();
+	}
+	
+	private String conflictRespGen(String res, int transacId) {
+		StringBuilder buffer = new StringBuilder();
+		buffer.append(InstrCode.EXE_RESP + " 0 ");
+		HashMap<Integer, Integer> lockInfo = locks.get(res);
+		Set<Map.Entry<Integer, Integer>> entries = lockInfo.entrySet();
+		buffer.append("{");
+		int counter = 0;
+		for(Map.Entry<Integer, Integer> entry : entries) {
+			buffer.append(entry.getKey());
+			counter ++;
+			if(counter < entries.size()) {
+				buffer.append(",");
+			}
+		}
+		buffer.append("} ");
+		buffer.append(transacId);
+		return buffer.toString();
+	}
+	
+	/**
+	 * first time lock a resource
+	 * @return void
+	 */
+	private void newLock(int transacId, String res, boolean isExclusive) {
+		HashMap<Integer, Integer> lockInfo = new HashMap<Integer, Integer>();
+		lockInfo.put(transacId, isExclusive ? 1 : 0);
+		locks.put(res, lockInfo);
+		return;
 	}
 	
 	/**
@@ -30,15 +60,32 @@ public class LockManager {
 	 * @return String NULL: lock retrieved; "conflict: T1, T2": T1 is conflict with T2, T2 holds the lock
 	 */
 	public String lock(int transacId, String res, boolean isExclusive) {
+		//if there're locks on this resource
 		if(locks.containsKey(res)) {
-			ArrayList<String> keyInfo = locks.get(res);
-			String[] tmp = keyInfo.get(0).split(":");
-			int lockType = Integer.parseInt(tmp[1]);
-			if(lockType == 1 || isExclusive) {
-				return InstrCode.EXE_RESP + " 0 " + locks.get(res).toString() + " " + transacId;
+			int lockType = 0;
+			HashMap<Integer, Integer> lockInfo = locks.get(res);
+			Set<Map.Entry<Integer, Integer>> entries = lockInfo.entrySet();
+			for(Map.Entry<Integer, Integer> entry : entries) {
+				lockType = entry.getValue();
+				break;
+			}										 
+			if(lockInfo.containsKey(transacId)) {
+				if((lockInfo.get(transacId) == 0 && lockInfo.size() == 1) || lockInfo.get(transacId) == 1) {
+					lockInfo.put(transacId, isExclusive ? 1:0);
+					return null;
+				}
+				else if(!isExclusive){
+					return null;
+				}
+				else {
+					return conflictRespGen(res, transacId);
+				}
+			}
+			else if(lockType == 1 || isExclusive) {
+				return conflictRespGen(res, transacId);
 			}
 			else {
-				keyInfo.add(lockGen(transacId, isExclusive));
+				lockInfo.put(transacId, 0);
 				return null;
 			}
 		}
@@ -58,26 +105,18 @@ public class LockManager {
 	}
 	
 	/**
-	 * unlock a certain resource
-	 * @param res resource name
-	 */
-	public void unlockRes(String res) {
-		locks.remove(res);
-	}
-	
-	/**
 	 * unlock all resources locked by a transaction T
 	 * @param int transacId
 	 */
-//	public void unlockTransac(int transacId) {
-//		Set<Map.Entry<String, Integer>> entries = locks.entrySet();
-//		for(Map.Entry<String, Integer> entry : entries) {
-//			int id = entry.getValue();
-//			if(id == transacId) {
-//				locks.remove(entry.getKey());
-//			}
-//		}
-//	}
+	public void unlockTransac(int transacId) {
+		Set<Map.Entry<String, HashMap<Integer, Integer>>> entries = locks.entrySet();
+		for(Map.Entry<String, HashMap<Integer, Integer>> entry : entries) {
+			entry.getValue().remove(entry.getKey());
+			if(entry.getValue().isEmpty()) {
+				locks.remove(entry.getKey());
+			}
+		}
+	}
 	
 	/**
 	 * lock certain resource for reading
@@ -92,19 +131,18 @@ public class LockManager {
 	 */
 	public void clearLocks() {
 		locks.clear();
+		recoverLocks.clear();
 	}
+	
+
 	
 	/**
-	 * first time lock a resource
-	 * @return
+	 * test main
 	 */
-	private void newLock(int transacId, String res, boolean isExclusive) {
-		ArrayList<String> newLock = new ArrayList<String>();
-		newLock.add(lockGen(transacId, isExclusive));
-		locks.put(res, newLock);
-	}
-	
-	private String lockGen(int transacId, boolean isExclusive) {
-		return Integer.toString(transacId) + ":" + (isExclusive ? 1 : 0);
+	public static void main(String[] args) {
+		LockManager lm = new LockManager();
+		System.out.println(lm.lock(0, "X1", false));
+		System.out.println(lm.lock(1, "X1", false));
+		System.out.println(lm.lock(0, "X1", true));
 	}
 }
